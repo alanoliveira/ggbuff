305ed0973653bff6926232aed72d90e105c89b79
Alan Oliveira <alan.oliveira89@gmail.com>
2022-06-11 15:35:12 +0900

    Add ggxrd.com API class

---
+++ app/services/ggxrd_dot_com/api.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module GgxrdDotCom
+  class Api
+    class ApiError < StandardError; end
+
+    attr_reader :cli
+
+    def initialize(cli)
+      @cli = cli
+    end
+
+    def login(login, password)
+      login_resp = cli.login(login, password)
+      login_resp.is_a?(Net::HTTPFound)
+    end
+
+    def my_id?(id)
+      # when you are logged and try to access you member_profile_view it will redirect you to profile page
+      cli.member_profile_view(id).is_a?(Net::HTTPFound)
+    end
+
+    def profile
+      profile_resp = cli.profile
+      raise ApiError unless profile_resp.is_a?(Net::HTTPOK)
+
+      parser = GgxrdDotCom::Parsers::ProfilePage.new(profile_resp.body)
+      GgxrdDotCom::Models::Profile.create(parser)
+    end
+
+    def matches(page=1)
+      play_log_resp = cli.play_log(page)
+      raise ApiError unless play_log_resp.is_a?(Net::HTTPOK)
+
+      parser = GgxrdDotCom::Parsers::PlayLogPage.new(play_log_resp.body)
+      GgxrdDotCom::Models::PlayLog.create(parser)
+    end
+
+    def search_player(player_name, page=1)
+      player_search_resp = cli.player_search(player_name, page)
+      raise ApiError unless player_search_resp.is_a?(Net::HTTPOK)
+
+      parser = GgxrdDotCom::Parsers::PlayerSearchPage.new(player_search_resp.body)
+      GgxrdDotCom::Models::PlayerSearch.create(parser)
+    end
+  end
+end
---
+++ spec/services/ggxrd_dot_com/api_spec.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe GgxrdDotCom::Api do
+  let(:instance) { described_class.new(cli) }
+  let(:cli) { instance_double(GgxrdDotCom::Client) }
+
+  shared_context "when ggxrd.com return a non 200 response" do
+    context do
+      let(:response) { Net::HTTPInternalServerError.new(1.0, "500", "Internal Server Error") }
+      it { expect { subject }.to raise_error GgxrdDotCom::Api::ApiError }
+    end
+  end
+
+  describe "#login" do
+    subject { instance.login("user", "password") }
+    let(:response) { Net::HTTPFound.new(1.0, "302", "Found") }
+
+    before { allow(cli).to receive(:login).and_return(response) }
+
+    it { is_expected.to be true }
+
+    context "when login fails" do
+      # Yes, it returns 200 when login fails
+      let(:response) { Net::HTTPOK.new(1.0, "200", "OK") }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe "#my_id?" do
+    subject { instance.my_id?(1) }
+    let(:response) { Net::HTTPFound.new(1.0, "302", "Found") }
+
+    before { allow(cli).to receive(:member_profile_view).and_return(response) }
+
+    it { is_expected.to be true }
+
+    context "when it is not" do
+      let(:response) { Net::HTTPOK.new(1.0, "200", "OK") }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe "#profile" do
+    subject { instance.profile }
+    let(:response) { Net::HTTPOK.new(1.0, "200", "OK") }
+
+    before do
+      allow(response).to receive(:body)
+      allow(cli).to receive(:profile).and_return(response)
+      allow(GgxrdDotCom::Parsers::ProfilePage).to receive(:new)
+      allow(GgxrdDotCom::Models::Profile).to receive(:create).and_return(GgxrdDotCom::Models::Profile.new)
+    end
+
+    it { is_expected.to be_a(GgxrdDotCom::Models::Profile) }
+
+    include_context "when ggxrd.com return a non 200 response"
+  end
+
+  describe "#matches" do
+    subject { instance.matches }
+    let(:response) { Net::HTTPOK.new(1.0, "200", "OK") }
+
+    before do
+      allow(response).to receive(:body)
+      allow(cli).to receive(:play_log).and_return(response)
+      allow(GgxrdDotCom::Parsers::PlayLogPage).to receive(:new)
+      allow(GgxrdDotCom::Models::PlayLog).to receive(:create).and_return(GgxrdDotCom::Models::PlayLog.new)
+    end
+
+    it { is_expected.to be_a(GgxrdDotCom::Models::PlayLog) }
+
+    include_context "when ggxrd.com return a non 200 response"
+  end
+
+  describe "#search_player" do
+    subject { instance.search_player("GG Player") }
+    let(:response) { Net::HTTPOK.new(1.0, "200", "OK") }
+
+    before do
+      allow(response).to receive(:body)
+      allow(cli).to receive(:player_search).and_return(response)
+      allow(GgxrdDotCom::Parsers::PlayerSearchPage).to receive(:new)
+      allow(GgxrdDotCom::Models::PlayerSearch).to receive(:create).and_return(GgxrdDotCom::Models::PlayerSearch.new)
+    end
+
+    it { is_expected.to be_a(GgxrdDotCom::Models::PlayerSearch) }
+
+    include_context "when ggxrd.com return a non 200 response"
+  end
+end
