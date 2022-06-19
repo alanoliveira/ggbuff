505ed528fa382f13caa707b2f25ab8916608a2c4
Alan Oliveira <alan.oliveira89@gmail.com>
2022-06-19 16:13:19 +0900

    Base layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_alert_class(type)
+    case type.to_s
+    when "notice"  then "alert-secondary"
+    when "error"   then "alert-danger"
+    else "alert-#{level}"
+    end
+  end
 end
