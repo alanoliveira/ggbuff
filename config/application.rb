4c21210ef3ac1aae14e6c538e13ff003ef059051
Alan Oliveira <alan.oliveira89@gmail.com>
2022-07-24 22:42:00 +0900

    Move ggxrd.com client to lib

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Ggbuff
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     #
+    config.eager_load_paths << Rails.root.join("lib")
 
     config.ggbuff = config_for(:ggbuff)
 
