5335972827862f41da84d02d5bd35a557af41807
Alan Oliveira <alan.oliveira89@gmail.com>
2022-07-30 13:22:57 +0900

    Fix database cleaner

--- app/services/database_cleaner.rb
+++ app/services/database_cleaner.rb
@@ -7,9 +7,9 @@ class DatabaseCleaner
 
   def run
     Rails.logger.info "[DATABASE_CLEANER] started (#{conf})"
-    clean_old_matches
-    clean_empty_matches_load_processes
-    clean_players_without_matches
+    clean_matches
+    clean_matches_load_processes
+    clean_players
     Rails.logger.info "[DATABASE_CLEANER] ended"
   end
 
@@ -17,7 +17,7 @@ class DatabaseCleaner
 
   attr_reader :conf
 
-  def clean_old_matches
+  def clean_matches
     return if old_matches_threashold.blank?
 
     Rails.logger.info "[DATABASE_CLEANER] clean_old_matches started"
@@ -32,7 +32,7 @@ class DatabaseCleaner
     conf[:clean_matches_older_than].seconds.ago if conf[:clean_matches_older_than].present?
   end
 
-  def clean_empty_matches_load_processes
+  def clean_matches_load_processes
     return if conf[:clean_empty_matches_load_processes].blank?
 
     Rails.logger.info "[DATABASE_CLEANER] clean_empty_matches_loader_processes started"
@@ -45,14 +45,21 @@ class DatabaseCleaner
     Rails.logger.info "[DATABASE_CLEANER] clean_empty_matches_loader_processes ended"
   end
 
-  def clean_players_without_matches
-    return unless conf[:clean_players_without_matches]
+  def clean_players
+    return if conf[:clean_players_without_matches].blank?
 
+    Rails.logger.info "[DATABASE_CLEANER] clean_players_without_matches started"
     Player.lock.all.each do |player|
-      if Match.where(player_id: player.id).or(Match.where(opponent_id: player.id)).count.zero?
+      if delete_player?(player)
         Rails.logger.info "[DATABASE_CLEANER] deleting #{player.inspect}"
         player.delete
       end
     end
+    Rails.logger.info "[DATABASE_CLEANER] clean_players_without_matches ended"
+  end
+
+  def delete_player?(player)
+    player.matches_load_processes.count.zero? &&
+      Match.where(player_id: player.id).or(Match.where(opponent_id: player.id)).count.zero?
   end
 end
